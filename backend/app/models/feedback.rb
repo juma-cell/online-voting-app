2633d429524efe24b0204a1a566062ec8a3f09f1
Epharus Ndumia <epharusndumia@gmail.com>
2023-07-27 14:19:10 +0300

    created validation and association

--- backend/app/models/feedback.rb
+++ backend/app/models/feedback.rb
@@ -1,3 +1,7 @@
 class Feedback < ApplicationRecord
   belongs_to :user
+  belongs_to :voting_event
+
+  validates :eventName, presence: true, length: { maximum: 255 }
+    validates :message, presence: true
 end
