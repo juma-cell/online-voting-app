b81bfaec92224732b79c6f09629c22c98d90148d
Epharus Ndumia <epharusndumia@gmail.com>
2023-07-27 13:16:53 +0300

    created feedback table

---
+++ backend/db/migrate/20230727101152_create_feedbacks.rb
@@ -0,0 +1,11 @@
+class CreateFeedbacks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :feedbacks do |t|
+      t.string :eventName
+      t.text :message
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
