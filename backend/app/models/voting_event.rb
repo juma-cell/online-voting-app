4e0afd8035c7788bcd44111a88a54e3d759f2cf4
Epharus Ndumia <epharusndumia@gmail.com>
2023-07-27 14:12:28 +0300

    added validations

--- backend/app/models/voting_event.rb
+++ backend/app/models/voting_event.rb
@@ -1,3 +1,10 @@
 class VotingEvent < ApplicationRecord
-  belongs_to :user
+  has_many :user_votes
+  has_many :candidates
+
+
+  validates :eventsName, presence: true, length: { maximum: 255 }
+  validates :eventsDescription, presence: true
+  validates :duration, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  
 end
