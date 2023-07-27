ee2b6bdd851eff1e88820f4aa9e35ba10b2c2f83
Martin <Martin@Martin>
2023-07-27 19:12:27 +0300

    done

--- backend/app/controllers/candidates_controller.rb
+++ backend/app/controllers/candidates_controller.rb
@@ -1,70 +1,20 @@
 class CandidatesController < ApplicationController
-  before_action :set_candidate, only: %i[ show edit update destroy ]
-
-  # GET /candidates or /candidates.json
-  def index
-    @candidates = Candidate.all
-  end
+  before_action :set_candidate, only: [:show]
 
   # GET /candidates/1 or /candidates/1.json
   def show
-  end
-
-  # GET /candidates/new
-  def new
-    @candidate = Candidate.new
-  end
-
-  # GET /candidates/1/edit
-  def edit
-  end
-
-  # POST /candidates or /candidates.json
-  def create
-    @candidate = Candidate.new(candidate_params)
-
-    respond_to do |format|
-      if @candidate.save
-        format.html { redirect_to candidate_url(@candidate), notice: "Candidate was successfully created." }
-        format.json { render :show, status: :created, location: @candidate }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @candidate.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /candidates/1 or /candidates/1.json
-  def update
-    respond_to do |format|
-      if @candidate.update(candidate_params)
-        format.html { redirect_to candidate_url(@candidate), notice: "Candidate was successfully updated." }
-        format.json { render :show, status: :ok, location: @candidate }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @candidate.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /candidates/1 or /candidates/1.json
-  def destroy
-    @candidate.destroy
-
-    respond_to do |format|
-      format.html { redirect_to candidates_url, notice: "Candidate was successfully destroyed." }
-      format.json { head :no_content }
+    candidate = Candidate.find_by(id: params[:id]) # value or nil
+    if candidate
+      render json: candidate.as_json(include: [:user_vote, :voting_event])
+    else
+      render json: { error: "Candidate not found" }, status: :not_found
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_candidate
-      @candidate = Candidate.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def candidate_params
-      params.require(:candidate).permit(:name, :party, :manifesto, :image_url)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_candidate
+    @candidate = Candidate.find(params[:id])
+  end
 end
