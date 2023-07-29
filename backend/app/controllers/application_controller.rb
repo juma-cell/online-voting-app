aaf24398d7fec1aa4443236dca476abfefbb4cb5
Epharus Ndumia <epharusndumia@gmail.com>
2023-07-29 04:58:21 +0300

    edited code

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+    #sskip_before_action :verify_authenticity_token, if: -> { request.format.json? }
+    #protect_from_forgery with: :null_session
 end
