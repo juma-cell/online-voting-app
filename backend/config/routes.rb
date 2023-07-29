29a09b8eaa2dcc8b3a063f328e76ccf480fbb195
Epharus Ndumia <epharusndumia@gmail.com>
2023-07-29 05:50:06 +0300

    installed gem active_model_serializers

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get "/logged_in", to:'users#logged_in'
   post "/login", to: 'sessions#login'
   delete '/logout', to: 'sessions#logout'
+ 
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
