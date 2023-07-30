513d90918279624c6a10bb1cab3cba0bc1057d40
Bliss <blissmaake140@gmail.com>
2023-07-30 16:23:44 +0300

    Updated React-Recoil

--- frontend/src/atom/userAtom.js
+++ frontend/src/atom/userAtom.js
@@ -1,6 +1,18 @@
 import { atom } from "recoil"
 
+const initialState = {
+  name: "",
+  dob: "",
+  fbn: "",
+  email: "",
+  mobileNumber: "",
+  password: "",
+  reEnterpassword: "",
+  aadharPassword: "",
+  loggedIn: false,
+}
+
 export const userState = atom({
   key: "userState", // unique ID (with respect to other atoms/selectors)
-  default: [], // default value (aka initial value)
+  default: initialState, // default value (aka initial value)
 })
