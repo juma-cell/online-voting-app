02a4dac93d0abffb74a116bd13a03d37c93bde15
Juma <juma.musa@student.moringaschool.com>
2023-07-30 23:27:36 +0300

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,34 @@
-import { Routes, Route } from "react-router-dom"
-import "./App.css"
-import Layout from "./Layout/Layout"
-import Home from "./Pages/Home"
-import Signup from "./Pages/Signup"
-import Profile from "./Pages/Profile"
-import Login from "./Pages/Login"
-import Election from "./Pages/Election"
-import Voting from "./Pages/Voting"
-import About from "./Pages/About"
-import NotificationList from "./Pages/NotificationList"
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Layout from "./Layout/Layout";
+import Home from "./Pages/Home";
+import Signup from "./Pages/Signup";
+// import Profile from "./Pages/Profile";
+import Login from "./Pages/Login";
+import Election from "./Pages/Election";
+import Voting from "./Pages/Voting";
+import About from "./Pages/About";
+import NotificationList from "./Pages/NotificationList";
+import AuthProvider from "./Context/AuthContext";
+
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/election" element={<Election />} />
-        <Route path="/voting" element={<Voting />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/notification" element={<NotificationList />} />
-
-      </Route>
-    </Routes>
-  )
+<AuthProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          {/* <Route path="/profile" element={<Profile />} /> */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/election" element={<Election />} />
+          <Route path="/voting" element={<Voting />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/notification" element={<NotificationList />} />
+        </Route>
+      </Routes>
+  </AuthProvider>
+  );
 }
 
-export default App
+export default App;
