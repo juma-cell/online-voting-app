8c4af0efb69103a7544ed92d59641f17afe6ef2a
Freddy <alfredmuleri569@gmail.com>
2023-07-31 14:19:43 +0300

    update

--- frontend/src/Layout/Footer.js
+++ frontend/src/Layout/Footer.js
@@ -37,28 +37,6 @@ function Footer() {
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
           <a href="https://flowbite.com/" className="hover:underline">
-            d
-          </a>
-          . All Rights Reserved.
-        </span>
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-            d
-          </a>
-          . All Rights Reserved.
-        </span>
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-            d
-          </a>
-          . All Rights Reserved.
-        </span>
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-            d
           </a>
           . All Rights Reserved.
         </span>
