6ef94960583aae01d7d7527dbc8941546a6a565d
Juma <juma.musa@student.moringaschool.com>
2023-08-01 10:17:43 +0300

    done

--- frontend/src/Layout/Navbar.js
+++ frontend/src/Layout/Navbar.js
@@ -13,26 +13,53 @@ function Navbar() {
           <img src={image1} className="h-8 mr-3" alt="Flowbite Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Online-Voting</span>
         </div>
-        <div className="space-x-4">
-          <Link to="/" className="text-gray-900 hover:text-blue-700 dark:text-white dark:hover:text-blue-500">Home</Link>
-          <Link to="/login" className="text-gray-900 hover:text-blue-700 dark:text-white dark:hover:text-blue-500">Login</Link>
-          <Link to="/signup" className="text-gray-900 hover:text-blue-700 dark:text-white dark:hover:text-blue-500">Signup</Link>
-          <Link to="/profile" className="text-gray-900 hover:text-blue-700 dark:text-white dark:hover:text-blue-500">Profile</Link>
-         <Link to="/addevent" className="text-gray-900 hover:text-blue-700 dark:text-white dark:hover:text-blue-500">Add Event</Link>
+        <div className="space-x-4 flex"> 
+          <Link
+            to="/"
+            className="block py-2 pl-3 pr-4 text-white bg-gradient-to-br from-orange-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 text-center mr-2 mb-2"
+          >
+            Home
+          </Link>
 
+          <Link
+            to="/login"
+            className="block py-2 pl-3 pr-4 text-white bg-gradient-to-br from-purple-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 text-center mr-2 mb-2"
+          >
+            Login
+          </Link>
+
+          <Link
+            to="/signup"
+            className="block py-2 pl-3 pr-4 text-white bg-gradient-to-br from-yellow-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 text-center mr-2 mb-2"
+          >
+            Signup
+          </Link>
+
+          <Link
+            to="/profile"
+            className="block py-2 pl-3 pr-4 text-white bg-gradient-to-br from-red-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 text-center mr-2 mb-2"
+          >
+            Profile
+          </Link>
+
+          <Link
+            to="/addevent"
+            className="block py-2 pl-3 pr-4 text-white bg-gradient-to-br from-yellow-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 text-center mr-2 mb-2"
+          >
+            Add Event
+          </Link>
         </div>
-         <div><button
-                    onClick={signout}
-                    className="block py-2 pl-3 pr-4 text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5  text-center mr-2 mb-2"
-                  >
-                    Log out
-                  </button> </div>
-                 
-                
+        <div>
+          <button
+            onClick={signout}
+            className="block py-2 pl-3 pr-4 text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 text-center mr-2 mb-2"
+          >
+            Log out
+          </button>
+        </div>
       </div>
     </nav>
   );
 }
 
 export default Navbar;
-
--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -4,16 +4,25 @@ import image3 from '../assets/image3.png';
 
 function Home() {
   return (
-    <div style={{ color: 'white', display: 'flex', alignItems: 'center' }}>        
-     <img src={image5} alt="" style={{ marginRight: '20px', width: '700px', height: '400px' }} />
+    <div style={{ color: 'white', display: 'flex', alignItems: 'center' }}>
+      {/* Card 1 */}
+      <div style={{ marginRight: '20px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', borderRadius: '8px' }}>
+        <img src={image5} alt="" style={{ width: '400px', height: '200px', borderRadius: '8px 8px 0 0' }} />
+        <div style={{ padding: '16px' }}>
+          <h2>Card Title 1</h2>
+          <p>Card description goes here.</p>
+        </div>
+      </div>
 
-    <img src={image3} alt="" style={{ marginRight: '90px', width: '700px', height: '400px' }} />
-
-
-
+      {/* Card 2 */}
+      <div style={{ marginRight: '90px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', borderRadius: '8px' }}>
+        <img src={image3} alt="" style={{ width: '400px', height: '200px', borderRadius: '8px 8px 0 0' }} />
+        <div style={{ padding: '16px' }}>
+          <h2>Card Title 2</h2>
+          <p>Card description goes here.</p>
+        </div>
+      </div>
     </div>
-
-   
   );
 }
 
