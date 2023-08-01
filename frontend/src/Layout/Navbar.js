439291442c64e9154cf9d2c900b81883191f1872
Juma <juma.musa@student.moringaschool.com>
2023-08-01 12:55:46 +0300

    done

--- frontend/src/Layout/Navbar.js
+++ frontend/src/Layout/Navbar.js
@@ -22,22 +22,27 @@ function Navbar() {
             Home
           </Link>
           {!current_user?.email && (
+            <>
             <Link
               to="/login"
               className="block py-2 pl-3 pr-4 text-white bg-gradient-to-br from-purple-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 text-center mr-2 mb-2"
             >
               Login
             </Link>
-          )}
 
-          {current_user?.email && (
-            <>
-              <Link
+             <Link
                 to="/signup"
                 className="block py-2 pl-3 pr-4 text-white bg-gradient-to-br from-orange-400 to-blue-600 hover:bg-gradient-to-bl font-medium rounded-lg text-sm px-5 text-center mr-2 mb-2"
               >
                 Signup
               </Link>
+            </>
+
+          )}
+
+          {current_user?.email && (
+            <>
+             
 
               <Link
                 to="/profile"
