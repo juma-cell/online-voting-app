15838e9e8fd6b57cb12f333f0712dd392f936c6c
Juma <juma.musa@student.moringaschool.com>
2023-08-01 13:04:47 +0300

    done

--- frontend/src/Layout/Navbar.js
+++ frontend/src/Layout/Navbar.js
@@ -15,12 +15,7 @@ function Navbar() {
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Online-Voting</span>
         </div>
         <div className="space-x-4 flex">
-          <Link
-            to="/"
-            className="block py-2 pl-3 pr-4 text-white bg-gradient-to-br from-orange-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 text-center mr-2 mb-2"
-          >
-            Home
-          </Link>
+         
           {!current_user?.email && (
             <>
             <Link
@@ -42,7 +37,12 @@ function Navbar() {
 
           {current_user?.email && (
             <>
-             
+              <Link
+            to="/"
+            className="block py-2 pl-3 pr-4 text-white bg-gradient-to-br from-orange-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 text-center mr-2 mb-2"
+          >
+            Home
+          </Link>
 
               <Link
                 to="/profile"
