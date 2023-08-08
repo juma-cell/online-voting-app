8ec9bfbbc73d0cd5a4bfbd32800962858c0082cc
Juma <juma.musa@student.moringaschool.com>
2023-08-09 00:07:12 +0300

    done

--- frontend/src/Pages/CandidateList.js
+++ frontend/src/Pages/CandidateList.js
@@ -1,67 +1,56 @@
-import React, { useContext, useEffect, useState } from "react"
-import { VoteContext } from "../Context/VoteContext"
-import { AuthContext } from "../Context/AuthContext"
-import { useParams, useNavigate } from "react-router-dom"
-import Swal from "sweetalert2"
+import React, { useContext, useEffect, useState } from "react";
+import { VoteContext } from "../Context/VoteContext";
+import { AuthContext } from "../Context/AuthContext";
+import { useParams, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 function Candidates() {
-  const [candidatesData, setCandidatesData] = useState([])
-  const { id } = useParams()
-  const nav = useNavigate()
-  const [selectedCandidateId, setSelectedCandidateId] = useState(null)
-  const [hasVoted, setHasVoted] = useState(false)
-  const { BaseUrl } = useContext(VoteContext)
-  const { current_user } = useContext(AuthContext)
-
-  // useEffect(() => {
-  //   fetch("/candidates")
-  //     .then((response) => response.json())
-  //     .then((data) => setCandidatesData(data))
-  //     .catch((error) => console.error("Error fetching candidates:", error))
-  // }, [])
+  const [candidatesData, setCandidatesData] = useState([]);
+  const { id } = useParams();
+  const nav = useNavigate();
+  const [selectedCandidateId, setSelectedCandidateId] = useState(null);
+  const [hasVoted, setHasVoted] = useState(false);
+  const { BaseUrl } = useContext(VoteContext);
+  const { current_user } = useContext(AuthContext);
 
   useEffect(() => {
-    // /candidates/by_voting_event/:voting_event_id
     fetch(`${BaseUrl}/candidates/by_voting_event/${id}`)
       .then((res) => res.json())
       .then((EventCandidates) => {
-        console.log(EventCandidates)
-        setCandidatesData(EventCandidates)
-      })
-  }, [BaseUrl, candidatesData?.length, id])
+        console.log(EventCandidates);
+        setCandidatesData(EventCandidates);
+      });
+  }, [BaseUrl, id]);
 
   const handleVote = (e, candidate_id, user_id, event_id) => {
-    e.preventDefault()
-    // Send the vote to the server
-    console.log(user_id)
-    fetch(`${BaseUrl}/user_votes`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        candidate_id: candidate_id,
-        user_id: user_id,
-        voting_event_id: event_id,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle the response if necessary (e.g., show a success message)
+    e.preventDefault();
 
-        console.log("Vote submitted:", data)
-        setHasVoted(true) // Update the state to indicate that the user has voted
-        // addVote(selectedCandidateId) // Call the addVote function to update the voting context
-        // console.log("selected: ")
-        // console.log(
-        //   candidatesData.filter((obj) => obj.id === selectedCandidateId)[0]
-        //     .userName
-        // )
-        // console.log(selectedCandidateId)
-
-        nav(`/voting_events/${event_id}`)
-        Swal.fire("Success", "vote submitted", "success")
+    if (hasVoted && selectedCandidateId === candidate_id) {
+      // If user has already voted for the same candidate, unselect
+      setSelectedCandidateId(null);
+      setHasVoted(false);
+    } else {
+      // Send the vote to the server
+      fetch(`${BaseUrl}/user_votes`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          candidate_id: candidate_id,
+          user_id: user_id,
+          voting_event_id: event_id,
+        }),
       })
-      .catch((error) => console.error("Error submitting vote:", error))
-  }
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Vote submitted:", data);
+          setSelectedCandidateId(candidate_id);
+          setHasVoted(true);
+          nav(`/voting_events/${event_id}`);
+          Swal.fire("Success", "Vote submitted", "success");
+        })
+        .catch((error) => console.error("Error submitting vote:", error));
+    }
+  };
 
   return (
     <div>
@@ -74,25 +63,20 @@ function Candidates() {
           >
             <div className="bg-white rounded-lg shadow-md p-6">
               <p className="text-gray-800">Name: {candidate.userName}</p>
-              {/* Add other candidate information */}
               <p className="text-gray-800">Role: {candidate.role}</p>
               {hasVoted ? (
                 candidate.id === selectedCandidateId ? (
-                  <p>{`You have already voted for ${
-                    candidatesData.filter(
-                      (obj) => obj.id === selectedCandidateId
-                    )[0].userName
-                  }.`}</p>
+                  <p>You have already voted for {candidate.userName}.</p>
                 ) : (
-                  <p>{`You have already voted.`}</p>
+                  <p>You have already voted.</p>
                 )
               ) : (
                 <button
-                  onClick={(e) => {
-                    e.preventDefault()
-                    setSelectedCandidateId(candidate.id)
-                  }}
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  onClick={(e) => handleVote(e, candidate.id, current_user.id, id)}
+                  className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${
+                    selectedCandidateId === candidate.id ? "opacity-50" : ""
+                  }`}
+                  disabled={selectedCandidateId !== null}
                 >
                   Vote for {candidate.userName}
                 </button>
@@ -101,18 +85,8 @@ function Candidates() {
           </div>
         ))}
       </div>
-      {selectedCandidateId && !hasVoted && (
-        <button
-          onClick={(e) => {
-            handleVote(e, selectedCandidateId, current_user.id, id)
-          }}
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4"
-        >
-          Confirm Vote
-        </button>
-      )}
     </div>
-  )
+  );
 }
 
-export default Candidates
+export default Candidates;
--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,83 +1,3 @@
-// import React, { useContext, useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
-// import { VoteContext } from "../Context/VoteContext";
-// import Header from "../components/Header";
-
-// function Home() {
-//   const { fetchEventsData, BaseUrl } = useContext(VoteContext);
-//   const [votingEvents, setVotingEvents] = useState([]);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await fetch(`${BaseUrl}/voting_events`, {
-//           method: "GET",
-//           headers: { "Content-Type": "application/json" },
-//         });
-
-//         const data = await response.json();
-//         setVotingEvents(data);
-//       } catch (error) {
-//         console.error("Error fetching data:", error);
-//       }
-//     };
-
-//     fetchData();
-//   }, [BaseUrl]);
-
-//   return (
-//     <>
-//       <div className="home_header w-full bg-black">
-//         <Header />
-//       </div>
-//       <div className="mt-20">
-//         <h2 className="text-xl font-semibold text-blue-800 mb-2 uppercase text-center">
-//           Available Events
-//         </h2>
-//         {votingEvents?.length > 0 ? (
-//           votingEvents.map((votingEvent, index) => (
-//             <article
-//               key={index}
-//               className="rounded-xl border-2 border-gray-100 text-white mb-8"
-//             >
-//               <Link
-//                 to={`/voting_events/${votingEvent.id}`}
-//                 className="container mx-auto px-4 text-[10px] font-medium sm:text-xs"
-//               >
-//                 <div className="flex items-start gap-4 p-4 sm:p-6 lg:p-8">
-//                   <div>
-//                     <h1 className="font-large sm:text-lg">
-//                       {votingEvent.eventsName}
-//                     </h1>
-//                     <div className="mt-2 sm:flex sm:items-center sm:gap-2">
-//                       <div className="flex items-center gap-1 text-gray-500"></div>
-//                       <span className="hidden sm:block" aria-hidden="true">
-//                         &middot;
-//                       </span>
-//                       <p className="hidden sm:block lg:text-xl sm:text-green-400">
-//                         {votingEvent.eventsDescription}
-//                       </p>
-//                     </div>
-//                     <div className="mt-2">
-//                       Event Duration: {votingEvent.duration} hrs
-//                     </div>
-//                     <div className="mt-2">
-//                       Available Candidates: {votingEvent.availableCandidates ? votingEvent.availableCandidates.length : 0}
-//                     </div>
-//                   </div>
-//                 </div>
-//               </Link>
-//             </article>
-//           ))
-//         ) : (
-//           <p>No voting events found.</p>
-//         )}
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Home;
 import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { VoteContext } from "../Context/VoteContext";
@@ -105,16 +25,18 @@ function Home() {
     fetchData();
   }, [BaseUrl]);
 
-  const calculateTimeRemaining = (duration) => {
-    const eventDurationMillis = duration * 60 * 60 * 1000; // Convert hours to milliseconds
-    const currentTime = new Date().getTime();
-    const endTimeMillis = currentTime + eventDurationMillis;
-    const timeRemainingMillis = Math.max(0, endTimeMillis - currentTime);
-    const hours = Math.floor((timeRemainingMillis / (1000 * 60 * 60)) % 24);
-    const minutes = Math.floor((timeRemainingMillis / (1000 * 60)) % 60);
-    const seconds = Math.floor((timeRemainingMillis / 1000) % 60);
-    return { hours, minutes, seconds };
-  };
+  useEffect(() => {
+    const id = "your_voting_event_id"; // Replace with the actual voting event id
+    fetch(`${BaseUrl}/candidates/by_voting_event/${id}`)
+      .then((res) => res.json())
+      .then((EventCandidates) => {
+        console.log(EventCandidates);
+        // Assuming you have a setCandidatesData function to set the candidates data
+        // setCandidatesData(EventCandidates);
+      });
+  }, [BaseUrl]);
+
+  console.log(votingEvents);
 
   return (
     <>
@@ -128,7 +50,6 @@ function Home() {
         {votingEvents?.length > 0 ? (
           votingEvents.map((votingEvent, index) => {
             const eventExpired = new Date(votingEvent.endTime) < new Date();
-            const timeRemaining = calculateTimeRemaining(votingEvent.duration);
 
             return (
               <article
@@ -150,12 +71,7 @@ function Home() {
                         <div className="flex items-center gap-1 text-gray-500">
                           {eventExpired ? (
                             <span className="text-red-500">Expired</span>
-                          ) : (
-                            <span>
-                              {timeRemaining.hours}h {timeRemaining.minutes}m{" "}
-                              {timeRemaining.seconds}s left
-                            </span>
-                          )}
+                          ) : null}
                         </div>
                         <span
                           className={`hidden sm:block ${
@@ -173,15 +89,10 @@ function Home() {
                           {votingEvent.eventsDescription}
                         </p>
                       </div>
-                      <div className="mt-2">
-                        Event Duration: {votingEvent.duration} hrs
-                      </div>
-                      <div className="mt-2">
-                        Available Candidates:{" "}
-                        {votingEvent.availableCandidates
-                          ? votingEvent.availableCandidates.length
-                          : 0}
-                      </div>
+                      <span className="text-green-500">
+                        Available Candidates:
+                      </span>{" "}
+                      Click on the event to view the candidates
                     </div>
                   </div>
                 </Link>
