b31d03a36333236b9c6d040717dd4f69a8a8f44e
Juma <juma.musa@student.moringaschool.com>
2023-08-11 12:22:55 +0300

    done

--- client/src/Context/AuthContext.js
+++ client/src/Context/AuthContext.js
@@ -1,15 +1,16 @@
-import React, { createContext, useEffect, useState } from "react"
-import Swal from "sweetalert2"
-import { useNavigate } from "react-router-dom"
+import React, { createContext, useEffect, useState } from "react";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
-export const AuthContext = createContext()
+// Create the AuthContext
+export const AuthContext = createContext();
 
+// AuthProvider component
 export default function AuthProvider({ children }) {
-  const nav = useNavigate()
-  const [current_user, setCurrentUser] = useState(null)
-  const [onChange, setOnChange] = useState(true)
+  const nav = useNavigate();
+  const [current_user, setCurrentUser] = useState(null);
+  const [onChange, setOnChange] = useState(true);
 
-  // Function to handle user login
   const login = (email, password) => {
     fetch("/login", {
       method: "POST",
@@ -20,44 +21,42 @@ export default function AuthProvider({ children }) {
       .then((res) => res.json())
       .then((response) => {
         if (response.message) {
-          setCurrentUser(response.user)
-          nav("/")
-          Swal.fire("Success", response.message, "success")
-          setOnChange(!onChange)
+          setCurrentUser(response.user);
+          nav("/");
+          Swal.fire("Success", response.message, "success");
+          setOnChange(!onChange);
         } else {
           Swal.fire(
             "Error",
             "Login failed. Please check your credentials.",
             "error"
-          )
+          );
         }
       })
       .catch((error) => {
-        console.error("Error logging in:", error)
-      })
-  }
+        console.error("Error logging in:", error);
+      });
+  };
 
-  // Function to handle user logout
   const signout = () => {
     fetch("/logout", {
       method: "DELETE",
     })
       .then((res) => res.json())
       .then((response) => {
-        setCurrentUser(null)
-        setOnChange(!onChange)
+        setCurrentUser(null);
+        setOnChange(!onChange);
 
         if (response.message) {
-          nav("/login")
-          Swal.fire("Success", response.message, "success")
+          nav("/login");
+          Swal.fire("Success", response.message, "success");
         } else {
-          nav("/login")
-          Swal.fire("Success", response.success, "success")
+          nav("/login");
+          Swal.fire("Success", response.success, "success");
         }
-      })
-  }
+      });
+  };
 
-  // Function to handle user registration
   const signup = (firstName, lastName, userName, email, password) => {
     fetch("/users", {
       method: "POST",
@@ -67,21 +66,20 @@ export default function AuthProvider({ children }) {
       .then((res) => res.json())
       .then((response) => {
         if (response.error) {
-          Swal.fire("Error", response.error, "error")
+          Swal.fire("Error", response.error, "error");
         } else if (response.success) {
-          nav("/login")
-          Swal.fire("Success", response.success, "success")
-          setOnChange(!onChange)
+          nav("/login");
+          Swal.fire("Success", response.success, "success");
+          setOnChange(!onChange);
         } else {
-          Swal.fire("Error", "Something went wrong", "error")
+          Swal.fire("Error", "Something went wrong", "error");
         }
       })
       .catch((error) => {
-        console.error("Error registering user:", error)
-      })
-  }
+        console.error("Error registering user:", error);
+      });
+  };
 
-  // Function to handle changing the user's password
   const changePassword = (currentPassword, newPassword) => {
     fetch("/changepassword", {
       method: "PUT",
@@ -92,20 +90,19 @@ export default function AuthProvider({ children }) {
       .then((res) => res.json())
       .then((response) => {
         if (response.success) {
-          Swal.fire("Success", response.success, "success")
+          Swal.fire("Success", response.success, "success");
         } else if (response.error) {
-          Swal.fire("Error", response.error, "error")
+          Swal.fire("Error", response.error, "error");
         } else {
-          Swal.fire("Error", "Something went wrong", "error")
+          Swal.fire("Error", "Something went wrong", "error");
         }
       })
       .catch((error) => {
-        console.error("Error changing password:", error)
-      })
-  }
+        console.error("Error changing password:", error);
+      });
+  };
 
   useEffect(() => {
-    // Check if the user is logged in when the component mounts
     fetch("/logged_in", {
       method: "GET",
       headers: { "Content-Type": "application/json" },
@@ -113,24 +110,30 @@ export default function AuthProvider({ children }) {
     })
       .then((res) => res.json())
       .then((response) => {
-        console.log("current", response)
         if (response.user) {
-          setCurrentUser(response.user)
+          setCurrentUser(response.user);
         } else {
-          setCurrentUser(null)
+          setCurrentUser(null);
         }
       })
-  }, [onChange])
+      .catch((error) => {
+        console.error("Error checking login status:", error);
+      });
+  }, [onChange]);
 
+  // Define the context data
   const contextData = {
     login,
     signup,
     current_user,
     signout,
     changePassword,
-  }
+  };
 
+  // Provide the context data to children components
   return (
-    <AuthContext.Provider value={contextData}>{children}</AuthContext.Provider>
-  )
+    <AuthContext.Provider value={contextData}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
