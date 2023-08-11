55fffd413292f52cf7d2590e6c446063c68f8add
juma-cell <126476506+juma-cell@users.noreply.github.com>
2023-08-11 09:07:16 +0300

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,34 +1,15 @@
-# #!/usr/bin/env bash
-# # exit on error
-# set -o errexit
-
-# # Build commands for front end to create the production build
-# rm -rf public
-# npm install --prefix client && npm run build --prefix client
-# cp -a client/build/. public/
-
-# # Build commands for back end
-# gem install bundler
-# bundle install
-# bundle exec rails db:migrate 
-# bundle exec rails db:seed # if you have seed data, run this command for the initial deploy only to avoid duplicate records
-
-#!/bin/bash
+#!/usr/bin/env bash
 # exit on error
 set -o errexit
 
-# Navigate to the client directory
-cd client
-
-# Install dependencies and build the front-end
-npm install
-npm run build
-
-# Navigate back to the root directory
-cd ..
+# Build commands for front end to create the production build
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
 
 # Build commands for back end
-# Use the full path to the bundle command (replace '/path/to/bundle' with the actual path)
+gem install bundler
 bundle install
-bundle exec rake db:migrate
-bundle exec rake db:seed
+bundle exec rails db:migrate 
+bundle exec rails db:seed # if you have seed data, run this command for the initial deploy only to avoid duplicate records
+
